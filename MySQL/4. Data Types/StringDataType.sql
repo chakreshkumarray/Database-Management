c9ed61e3a0f519321acbea2fa593759771437ba6
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-17 20:27:00 +0530

    Delete StringDataType.sql

--- MySQL/4. Data Types/StringDataType.sql
+++
@@ -1,9 +0,0 @@
-create database StringTypes;
-use StringTypes;
-
-create table StringTypesTest(
-employees_id varchar(50),
-college_id varchar(10),
-first_name varchar(20),
-last_name varchar(14)
-);
