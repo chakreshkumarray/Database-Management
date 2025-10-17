67592c85ae1816b4f20b574100b4eca7ab8882ea
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-17 20:20:23 +0530

    Create Numeric.sql

---
+++ MySQL/4. Data Types/Numeric.sql
@@ -0,0 +1,12 @@
+create database Numbers;
+use Numbers;
+
+create table test1(num1 int);
+insert into test1 values(12344);
+
+select * from test1;
+
+create table test2(num decimal);
+insert into test2 values(444.44);
+
+select * from test2;
