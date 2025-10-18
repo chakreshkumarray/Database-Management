9a0e634950c5a2bbf4a0517ac93cb55b643caf7f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-18 21:02:22 +0530

    Create AlterTable.sql

---
+++ MySQL/6. Table Commands/AlterTable.sql
@@ -0,0 +1,57 @@
+create database AlterTable;
+use AlterTable;
+
+create table Department(
+employee_id int primary key auto_increment,
+first_name varchar(50),
+last_name varchar(50),
+hire_date date default (current_date()),
+email varchar(100) unique,
+phone_number varchar(100) unique,
+salary decimal(10,2) check (salary > 0.0),
+employee_status enum('active','on leave','terminated') default 'active',
+created_at timestamp default CURRENT_TIMESTAMP,
+updated_at timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+);
+
+INSERT INTO Department(first_name, last_name, hire_date, email, phone_number, salary, employee_status)
+VALUES('sk','kumar','2004-01-01','sk.kumar@gmail.com','7055278548',71893.100,'on leave'),
+       ('dk','kumar','2004-01-01','dk.kumar@gmail.com','4055278541',21893.100,'active'),
+       ('hk','kumar','2004-01-01','hk.kumar@gmail.com','3055278542',31893.100,'terminated'),
+       ('lk','kumar','2004-01-01','lk.kumar@gmail.com','2055278543',41893.100,'on leave'),
+       ('ck','kumar','2004-01-01','ck.kumar@gmail.com','1055278544',51893.100,'active');
+       
+-- Add Column --       
+ Alter table Department
+ add column emergency_contact varchar(100);
+ 
+-- Modify Data type --
+Alter table Department
+modify column emergency_contact varchar(100);
+
+-- Rename Column --
+Alter table Department
+rename column emergency_contact to em_contact;
+
+-- Drop Column --
+Alter table Department
+drop column emergency_contact;
+
+-- Add Constraints --
+Alter table Department
+add check (emergency_contact regexp '^[A-Za-z ]+: [0-9+-]+$');
+
+-- Not Null --
+Alter table Department
+modify column emergency_contact varchar(100) not null;
+
+-- Connect --
+Alter table Department
+add column department_id int;
+
+-- Department Name Add--
+insert into Departments(department_name,location)
+values('IT','A'),
+      ('HR', 'B');
+
+select * from Department;
