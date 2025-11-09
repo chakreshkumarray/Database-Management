8fea6debdd63e3e533d5b53440b4b0a8d3101581
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-09 12:18:04 +0530

    Create String.sql

---
+++ MySQL/1. MySQL Tutorial/4. Data Types/String.sql
@@ -0,0 +1,11 @@
+create database Strings;
+use Strings;
+
+create table Strings(
+employees_id varchar(50),
+college_id varchar(10),
+first_name varchar(20),
+last_name varchar(14)
+);
+
+select * from Strings;
